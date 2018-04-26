f1ab84e2eb8a5fbb8a17b528df911fc5aadf40a8
Viet Nguyen <viet@elecom.com.au>
2018-04-26 15:47:29 +1000

    Add tabindex to all elements, add link and keydown event for show/hide button

--- js/controllers.js
+++ js/controllers.js
@@ -7,19 +7,29 @@ angular
         $scope.BusServicesData = BusServicesData.data
       });
 
-    $scope.toggleBusRoutes = function(e) {
+    $scope.toggleBusRoutesClick = function(e) {
+      var p = angular.element(e.target).parent();
+      $scope.toggleBusRoutesPanel(p)
+    };
+
+    $scope.toggleBusRoutesKeyDown = function(e) {
       // Only type enter key (keycode = 13) to toggle show/hide
       if(e.keyCode === 13) {
         var p = angular.element(e.target).parent();
-
-        if(p.hasClass('hide')) {
-          // display content
-          p.removeClass('hide').addClass('show')
-        } else if(p.hasClass('show')) {
-          // hide content
-          p.removeClass('show').addClass('hide')
-        }
+        $scope.toggleBusRoutesPanel(p)
       }
 
     };
+
+    $scope.toggleBusRoutesPanel = function(element) {
+      
+
+      if(element.hasClass('hide')) {
+        // display content
+        element.removeClass('hide').addClass('show')
+      } else if(element.hasClass('show')) {
+        // hide content
+        element.removeClass('show').addClass('hide')
+      }
+    }
   });
